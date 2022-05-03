1f1639df4fcef1ae25422c34ef2748d2d2440686
tkmktkhr <mukku.wordpress@gmail.com>
2022-05-03 18:25:33 +0900

    first api

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+
+#[get("/{id}/{name}")]
+async fn sample_get(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {App::new().service(sample_get).service(index)})
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
