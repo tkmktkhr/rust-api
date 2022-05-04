cf2d31b054bb0c62e0afefb16fde71a25121a184
tkmktkhr <mukku.wordpress@gmail.com>
2022-05-04 15:53:20 +0900

    create first module

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+// mod infrastructures;
 
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+mod routers {
+  use actix_web::{get, web, HttpResponse, Responder};
+
+  #[get("/")]
+  pub async fn index() -> impl Responder {
+      HttpResponse::Ok().body("Hello world!")
+      // web::Json(json!({ "temperature": 42.3 }))
+  }
+  
+  
+  #[get("/{id}/{name}")]
+  pub async fn sample_get(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello {}! id:{}", name, id))
+  }
 }
 
+// #[get("/")]
+// async fn index() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
 
-#[get("/{id}/{name}")]
-async fn sample_get(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
-  HttpResponse::Ok().body(format!("Hello {}! id:{}", name, id))
-}
+
+// #[get("/{id}/{name}")]
+// async fn sample_get(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+//   HttpResponse::Ok().body(format!("Hello {}! id:{}", name, id))
+// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {App::new().service(sample_get).service(index)})
+    // use router;
+    use routers;
+    
+    HttpServer::new(|| {App::new()
+        .service(routers::sample_get)
+        .service(routers::index)})
         .bind("127.0.0.1:8080")?
         .run()
         .await
