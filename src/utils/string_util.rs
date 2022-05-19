3db11539d5a46128eff593cac6e0b964362d1597
tkmktkhr <mukku.wordpress@gmail.com>
2022-05-19 22:09:02 +0900

    add test fn check_string_return_string

--- src/utils/string_util.rs
+++ src/utils/string_util.rs
@@ -1,5 +1,5 @@
 pub mod string_util {
-  // this fn should be like this -> original.unwrap_or_default()
+    // this fn should be like this -> original.unwrap_or_default()
     pub fn check_string_return_string(original: &Option<String>) -> String {
         match original {
             None => "".to_string(),
@@ -17,3 +17,28 @@ pub mod string_util {
 
 // &str -> slice - references, primitive
 // String -> Vector
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_string_return_string_return_empty_str() {
+        let none = None;
+        let res = string_util::check_string_return_string(&none);
+        assert_eq!(res, "");
+    }
+
+    #[test]
+    fn check_string_return_string_return_input_str() {
+        let str = String::from("str");
+        let res = string_util::check_string_return_string(&Some(str));
+        assert_eq!(res, "str");
+    }
+
+    // #[test]
+    #[actix_rt::test]
+    async fn test_return_user() {
+        assert_eq!(2 + 2, 4);
+    }
+}
