7da55bedb753a0b4ca7c2b1027e144bdd1b750a5
tkmktkhr <mukku.wordpress@gmail.com>
2022-06-03 08:23:31 +0900

    sample users api

--- src/infrastructures/router.rs
+++ src/infrastructures/router.rs
@@ -60,5 +60,16 @@ pub async fn create_user(body: web::Json<User>) -> impl Responder {
     let user_id = body.user_id;
     let name = &body.name;
     // TODO PostUsersController
-    web::Json(json!({ "id": user_id + 1, "name": name }))
+    let user1 = User {
+      user_id: user_id + 1,
+      name: name.to_string()
+    };
+    let user2 = User {
+      user_id: user_id + 2,
+      name: String::from("name2")
+    };
+    let users = [user1, user2];
+
+    // web::Json(json!({ "id": user_id + 1, "name": name }))
+    web::Json(users)
 }
