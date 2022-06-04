9f9420aa4867b442e74842528267f6dbf2595503
tkmktkhr <mukku.wordpress@gmail.com>
2022-06-04 13:46:02 +0900

    add serialize to user struct

--- src/interfaces/requests/create_user_request.rs
+++ src/interfaces/requests/create_user_request.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct User {
     pub user_id: u32,
     pub name: String,
