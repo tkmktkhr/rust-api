298f8d6d88e0f4b309381ee099fb61241cf2430d
tkmktkhr <mukku.wordpress@gmail.com>
2022-06-29 07:43:07 +0900

    modify the User struct params type

--- src/infrastructures/models/user.rs
+++ src/infrastructures/models/user.rs
@@ -2,11 +2,11 @@ use diesel::{Insertable, Queryable};
 
 // NOTE #[derive(Queryable)] will generate all of the code needed to load a Post struct from a SQL query.
 // #[derive(Queryable, Insertable)]
-#[derive(Queryable, PartialEq, Debug)]
 // #[table_name = "users"]
+#[derive(Queryable, PartialEq, Debug)]
 pub struct User {
     pub id: u32,
     pub first_name: String,
-    pub last_name: String,
-    pub email: String,
+    pub last_name: Option<String>,
+    pub email: Option<String>,
 }
