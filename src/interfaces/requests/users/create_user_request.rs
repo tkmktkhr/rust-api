981c71840b7a508b7b391a5d23a603e135fba07c
tkmktkhr <mukku.wordpress@gmail.com>
2022-07-26 07:51:04 +0900

    add some params CreateUserReq

--- src/interfaces/requests/users/create_user_request.rs
+++ src/interfaces/requests/users/create_user_request.rs
@@ -3,5 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize)]
 pub struct CreateUserReq {
     pub user_id: u32,
-    pub name: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: Option<String>,
 }
