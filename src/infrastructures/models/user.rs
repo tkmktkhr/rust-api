bb8f4ed7a8f0ba6b17db5d486317297176dc42ec
tkmktkhr <mukku.wordpress@gmail.com>
2022-07-31 11:44:08 +0900

    ass params for insert data

--- src/infrastructures/models/user.rs
+++ src/infrastructures/models/user.rs
@@ -1,7 +1,8 @@
-use diesel::Queryable;
+use diesel::{Insertable, Queryable};
 
 // NOTE #[derive(Queryable)] will generate all of the code needed to load a Post struct from a SQL query.
-#[derive(Queryable, PartialEq, Debug)]
+#[derive(Queryable, PartialEq, Debug, Insertable)]
+#[table_name = "users"]
 pub struct User {
     pub id: u32,
     pub first_name: String,
