f569090b334504d5e6aadee33bea7d182827ee91
tkmktkhr <mukku.wordpress@gmail.com>
2022-09-23 11:41:09 +0900

    fix response not found on get usre by id

--- src/infrastructures/router.rs
+++ src/infrastructures/router.rs
@@ -73,13 +73,12 @@ pub async fn get_user_by_id(path: web::Path<i32>) -> impl Responder {
 
     let res = match output {
         // TODO NOT Found
-        None => web::Json(obj),
-        // None => (
-        //     web::Json(ResponseStruct {
-        //         res: Res::NotFoundError(obj),
-        //     }),
-        //     http::StatusCode::CREATED,
-        // ),
+        None => (
+            web::Json(ResponseStruct {
+                res: Res::NotFoundError(obj),
+            }),
+            http::StatusCode::CREATED,
+        ),
         Some(response) => (
             web::Json(ResponseStruct { res: response }),
             // web::Json(ResponseStruct {
