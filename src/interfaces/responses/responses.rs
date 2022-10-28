b3d4adbef2569b8061a0b144de2302936559adad
tkmktkhr <mukku.wordpress@gmail.com>
2022-10-29 08:01:00 +0900

    check custom error enum works

--- src/interfaces/responses/responses.rs
+++ src/interfaces/responses/responses.rs
@@ -3,18 +3,33 @@ use serde::Serialize;
 // TODO abstract. Use <T>
 // TODO NotFoundError -> General Error.
 #[derive(Debug, Serialize)]
-pub enum Res<T> {
+pub enum Res<T, E> {
     FindUserOutputData(Option<T>),
-    NotFoundError(NotFoundError),
+    CustomError(E),
 }
+// pub enum Res<T> {
+//   FindUserOutputData(Option<T>),
+//   NotFoundError(NotFoundError),
+// }
 
 #[derive(Debug, Serialize)]
-pub struct ResponseStruct<T> {
-    pub res: Res<T>,
+pub struct ResponseStruct<T, E> {
+    pub res: Res<T, E>,
 }
 
 // Errors
 #[derive(Debug, Serialize)]
+pub enum CustomError<T> {
+    NotFoundError(NotFoundError),
+    BadRequestError(BadRequestError),
+}
+
+#[derive(Debug, Serialize)]
 pub struct NotFoundError {
+    pub msg: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct BadRequestError {
     pub msg: String,
 }
