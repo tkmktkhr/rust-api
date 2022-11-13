974ed7a9f46cd91f127f8d174c2edc49d8ec43b6
tkmktkhr <mukku.wordpress@gmail.com>
2022-11-14 07:37:53 +0900

    refactor response struct

--- src/interfaces/responses/responses.rs
+++ src/interfaces/responses/responses.rs
@@ -4,8 +4,9 @@ use serde::Serialize;
 // TODO NotFoundError -> General Error.
 #[derive(Debug, Serialize)]
 pub enum Res<T> {
-    OutputData(Option<T>),        // to general object.
-    NotFoundError(NotFoundError), // todo arrange all errors here?
+    OutputData(Option<T>), // to general object.
+    CustomError(CustomError),
+    // NotFoundError(NotFoundError), // todo arrange all errors here?
 }
 
 #[derive(Debug, Serialize)]
@@ -20,14 +21,13 @@ pub struct CustomErrorStruct<CustomError> {
     pub err: CustomError,
 }
 
-// #[derive(Debug, Serialize)]
-// pub enum CustomError {
-//     NotFoundError(NotFoundError),
-//     BadRequestError(BadRequestError),
-// }
+#[derive(Debug, Serialize)]
+pub struct CustomError {
+    pub msg: String,
+}
 
 #[derive(Debug, Serialize)]
-pub enum CustomError {
+pub enum CustomError1 {
     NotFoundError(NotFoundError),
     BadRequestError(BadRequestError),
 }
