bfee0e4405c6c4278e61a82d03ba2516c2480fae
tkmktkhr <mukku.wordpress@gmail.com>
2022-12-29 09:02:17 +0900

    update table bind diesel

--- src/infrastructures/models/user.rs
+++ src/infrastructures/models/user.rs
@@ -3,7 +3,7 @@ use rust_api::schema::users;
 
 // NOTE #[derive(Queryable)] will generate all of the code needed to load a Post struct from a SQL query.
 #[derive(QueryableByName, Queryable, Debug)]
-#[table_name = "users"]
+#[diesel(table_name = users)]
 pub struct User {
     pub id: u32,
     pub first_name: String,
@@ -12,7 +12,7 @@ pub struct User {
 }
 
 #[derive(Insertable)]
-#[table_name = "users"]
+#[diesel(table_name = users)]
 pub struct NewUser {
     // pub id: u32,
     pub first_name: String,
